60521641167e44f2defdf9e50ee3c69d887f22a1
Lasha Efremidze <efremidzel@hotmail.com>
2017-01-12 21:17:25 -0800

    BACKUP

--- Alarm/AppDelegate.swift
+++ Alarm/AppDelegate.swift
@@ -52,8 +52,7 @@ extension AppDelegate {
     func setup() {
         UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .alert]) { accepted, error in
             if accepted {
-                let newDate = Date(timeIntervalSinceNow: 15)
-                self.scheduleNotification(at: newDate)
+                self.scheduleNotification()
             } else {
                 print("Notification access denied.")
             }
@@ -64,12 +63,38 @@ extension AppDelegate {
         UNUserNotificationCenter.current().setNotificationCategories([category])
     }
     
-    func scheduleNotification(at date: Date) {
-        let calendar = Calendar(identifier: .gregorian)
-        let components = calendar.dateComponents(in: .current, from: date)
-        let newComponents = DateComponents(calendar: calendar, timeZone: .current, month: components.month, day: components.day, hour: components.hour, minute: components.minute)
+//    func scheduleNotification(at date: Date) {
+//        let calendar = Calendar(identifier: .gregorian)
+//        let components = calendar.dateComponents(in: .current, from: date)
+//        let newComponents = DateComponents(calendar: calendar, timeZone: .current, month: components.month, day: components.day, hour: components.hour, minute: components.minute)
+//        
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: newComponents, repeats: false)
+//        
+//        let content = UNMutableNotificationContent()
+//        content.title = "Tutorial Reminder"
+//        content.body = "Just a reminder to read your tutorial over at appcoda.com!"
+//        content.sound = UNNotificationSound.default()
+//        content.categoryIdentifier = "myCategory"
+//        
+//        let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+//        
+//        UNUserNotificationCenter.current().delegate = self
+//        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+//        UNUserNotificationCenter.current().add(request) {(error) in
+//            if let error = error {
+//                print("Uh oh! We had an error: \(error)")
+//            }
+//        }
+//        
+//        print("schedule")
+//    }
+    
+    func scheduleNotification() {
+        var components = DateComponents()
+        components.hour = 16
+        components.minute = 20
         
-        let trigger = UNCalendarNotificationTrigger(dateMatching: newComponents, repeats: false)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
         
         let content = UNMutableNotificationContent()
         content.title = "Tutorial Reminder"
@@ -87,7 +112,7 @@ extension AppDelegate {
             }
         }
         
-        print("schedule")
+        print("scheduled")
     }
     
 }
@@ -101,8 +126,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         print(response.actionIdentifier)
         if response.actionIdentifier == "remindLater" {
-            let newDate = Date(timeIntervalSinceNow: 15)
-            scheduleNotification(at: newDate)
+            scheduleNotification()
         }
     }
     
