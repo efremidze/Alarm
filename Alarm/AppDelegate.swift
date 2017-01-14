2bff0c2a11d62661b0b7ad6d0a824b45a30d06ff
Lasha Efremidze <efremidzel@hotmail.com>
2017-01-14 15:09:05 -0800

    updating notifications

--- Alarm/AppDelegate.swift
+++ Alarm/AppDelegate.swift
@@ -10,30 +10,12 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-//        setup()
-        
         return true
     }
     
 }
-
-//extension AppDelegate: UNUserNotificationCenterDelegate {
-//
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-//        print(notification)
-//    }
-//    
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-//        print(response.actionIdentifier)
-//        if response.actionIdentifier == "remindLater" {
-//            scheduleNotification()
-//        }
-//    }
-//    
-//}
--- Alarm/Notifications.swift
+++ Alarm/Notifications.swift
@@ -12,50 +12,59 @@ let UNC = UNUserNotificationCenter.current()
 
 struct Notifications {
     
-    func setup() {
+    enum `Type`: String {
+        case am, pm, day
+        
+        private func dateComponents() -> DateComponents {
+            switch self {
+            case .am:
+                var components = DateComponents()
+                components.hour = 4
+                components.minute = 20
+                return components
+            case .pm:
+                var components = DateComponents()
+                components.hour = 16
+                components.minute = 20
+                return components
+            case .day:
+                var components = DateComponents()
+                components.month = 4
+                components.day = 20
+                return components
+            }
+        }
+        
+        func schedule(completionHandler: ((Error?) -> Void)? = nil) {
+            UNC.add(identifier: rawValue, title: "420 Get High 🙃", dateComponents: dateComponents(), withCompletionHandler: completionHandler)
+        }
+    }
+    
+    static func start() {
         UNC.requestAuthorization(options: [.sound, .alert]) { accepted, error in
             if accepted {
-                self.scheduleNotification()
+                
             } else {
                 print("Notification access denied.")
             }
         }
-        
-//        let action = UNNotificationAction(identifier: "remindLater", title: "Remind me later", options: [])
-//        let category = UNNotificationCategory(identifier: "myCategory", actions: [action], intentIdentifiers: [], options: [])
-//        UNC.setNotificationCategories([category])
     }
     
-    func scheduleNotification() {
-//        UNC.delegate = self
+    static func reset() {
         UNC.removeAllPendingNotificationRequests()
-        
-        var components = DateComponents()
-        components.hour = 16
-        components.minute = 20
-        UNC.add(components) { error in
-            if let error = error {
-                print("Uh oh! We had an error: \(error)")
-            }
-        }
     }
     
 }
 
 extension UNUserNotificationCenter {
     
-    func add(_ dateComponents: DateComponents, withCompletionHandler completionHandler: ((Error?) -> Swift.Void)? = nil) {
-        add(notificationRequest(dateComponents: dateComponents), withCompletionHandler: completionHandler)
-    }
-    
-    private func notificationRequest(dateComponents: DateComponents) -> UNNotificationRequest {
+    func add(identifier: String, title: String, dateComponents: DateComponents, withCompletionHandler completionHandler: ((Error?) -> Void)? = nil) {
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         let content = UNMutableNotificationContent()
-        content.title = "Tutorial Reminder"
-        content.body = "Just a reminder to read your tutorial over at appcoda.com!"
-        content.sound = UNNotificationSound.default()
-//        content.categoryIdentifier = "myCategory"
-        return UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+        content.title = title
+        content.sound = .default()
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        add(request, withCompletionHandler: completionHandler)
     }
     
 }
