888220bfbdb25abf1790a0fba7535d9d816af77c
Lasha Efremidze <efremidzel@hotmail.com>
2017-02-26 02:08:46 -0800

    backup

--- Alarm WatchKit Extension/ComplicationController.swift
+++ Alarm WatchKit Extension/ComplicationController.swift
@@ -33,19 +33,13 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        switch complication.family {
-        case .modularSmall:
-            let template = CLKComplicationTemplateModularSmallSimpleImage()
-            template.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "weed")!)
-            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
-            handler(entry)
-        case .circularSmall:
-            let template = CLKComplicationTemplateCircularSmallSimpleImage()
-            template.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "weed")!)
-            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
-            handler(entry)
-        default:
-            handler(nil)
+        getLocalizableSampleTemplate(for: complication) { template in
+            if let template = template {
+                let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+                handler(entry)
+            } else {
+                handler(nil)
+            }
         }
     }
     
@@ -63,7 +57,22 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        switch complication.family {
+        case .modularSmall:
+            let template = CLKComplicationTemplateModularSmallSimpleImage()
+            template.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "weed")!)
+            handler(template)
+        case .circularSmall:
+            let template = CLKComplicationTemplateCircularSmallSimpleImage()
+            template.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "weed")!)
+            handler(template)
+        case .utilitarianSmall:
+            let template = CLKComplicationTemplateUtilitarianSmallSquare()
+            template.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "weed")!)
+            handler(template)
+        default:
+            handler(nil)
+        }
     }
     
 }
