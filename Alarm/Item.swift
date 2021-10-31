c2cbd10c927bb7c351369ab0e7e0dc685d277a89
Ameen Mustafa <amnmustafa15@gmail.com>
2021-10-31 11:56:14 -0600

    added "it's 4:20 somewhere" switch so notification is received every xx:20

--- Alarm/Item.swift
+++ Alarm/Item.swift
@@ -14,6 +14,6 @@ struct Item {
     let type: AlarmType
     
     static func all() -> [Item] {
-        return [Item(title: "4:20 AM", subtitle: nil, type: .am), Item(title: "4:20 PM", subtitle: nil, type: .pm), Item(title: "April 20", subtitle: nil, type: .day)]
+        return [Item(title: "4:20 AM", subtitle: nil, type: .am), Item(title: "4:20 PM", subtitle: nil, type: .pm), Item(title: "It's 4:20 somewhere", subtitle: nil, type: .hourly), Item(title: "April 20", subtitle: nil, type: .day)]
     }
 }
