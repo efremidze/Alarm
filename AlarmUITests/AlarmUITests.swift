35382a52ce77927d9516c2dfbf9779b4e15cf4bd
Lasha Efremidze <efremidzel@hotmail.com>
2022-01-07 01:02:31 -0800

    Github Actions (#7)

--- AlarmUITests/AlarmUITests.swift
+++ AlarmUITests/AlarmUITests.swift
@@ -34,7 +34,7 @@ class AlarmUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
-        let app = XCUIApplication()
+        _ = XCUIApplication()
         snapshot("0")
     }
     
--- Podfile
+++ Podfile
@@ -1,8 +1,8 @@
-# platform :ios, '10.0'
+platform :ios, '12.0'
 
 target 'Alarm' do
   use_frameworks!
-
+  
   pod 'RevealingSplashView', :git => 'https://github.com/PiXeL16/RevealingSplashView.git', :tag => '0.9.0'
   pod 'SkyFloatingLabelTextField', '~> 4.0'
 end
